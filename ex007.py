20c44999b4ae011cd2d576b3ae634265f6a730d4
Hugo Brito <hugomdmb@gmail.com>
2022-06-21 14:24:35 -0300

    Refactor of the exercises 006 ~ 007

--- ex007.py
+++ ex007.py
@@ -1,5 +1,5 @@
-# Pedir duas notas de um aluno e calcular a sua média.
-n1 = float(input('Primeira Nota:'))
-n2 = float(input('Segunda Nota:'))
-m = (n1+n2)/2
-print('\033[1mSua média é {:.1f}'.format(m))
+# Ask for two grades of a student and return it's average.
+firstGrade = float(input('First Grade:'))
+secondGrade = float(input('Second Grade:'))
+average = (firstGrade + secondGrade) / 2
+print(f'\033[1mYour average is {average:.1f}')
